2879c44b5027f1aee897c10145cc05738f8060fe
sharkvn1 <richberchannel01@gmail.com>
2023-11-18 01:12:19 +0700

    - same with member

---
+++ app/Http/Resources/V1/MajorResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MajorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'Id' => $this->Id,
+            'Name' => $this->Name,
+        ];
+    }
+}
