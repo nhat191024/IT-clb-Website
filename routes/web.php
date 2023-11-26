069c159d4dbe2caf6e62d2b22f6ab773b00d9815
sharkvn1 <richberchannel01@gmail.com>
2023-11-27 01:47:23 +0700

    + Test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/setup', function () {
+//     $test = [
+
+//     ];
+// });
