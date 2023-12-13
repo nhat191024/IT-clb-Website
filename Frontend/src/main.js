b8891ba4667e557d2ec7b1d5ccca828eec6ae163
sharkvn1 <richberchannel01@gmail.com>
2023-12-13 21:17:31 +0700

    + Setup Frontend

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import router from './routers'
 import App from './App.vue'
+import "./style.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
