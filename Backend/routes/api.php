913fd10b6eac38bea63a2430cf53936be30aec0b
sharkvn1 <richberchannel01@gmail.com>
2024-01-30 11:34:09 +0700

    Update: new api route for new database

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\V1\UsersController;
+use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\Api\V1\MajorsController;
 use App\Http\Controllers\Api\V1\CoursesController;
 use App\Http\Controllers\Api\V1\ProjectsController;
+use App\Http\Controllers\Api\V1\TypesController;
+use App\Http\Controllers\Api\V1\LanguagesController;
 use App\Http\Controllers\Api\V1\ProjectMembersController;
 use App\Http\Controllers\Api\V1\TasksController;
 use App\Http\Controllers\Api\V1\BlogsController;
@@ -27,8 +29,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-    Route::post('login', ['uses' => 'UsersController@login']);
-    Route::post('logout', ['uses' => 'UsersController@logout'])->middleware('auth:sanctum');
+    Route::post('login', ['uses' => 'UserController@login']);
+    Route::post('logout', ['uses' => 'UserController@logout'])->middleware('auth:sanctum');
     Route::get('quantity', ['uses' => 'TasksController@quantity'])->middleware('auth:sanctum');
     Route::get('prjId', ['uses' => 'ProjectMembersController@prjId'])->middleware('auth:sanctum');
     Route::get('projectsDelete/{id}', ['uses' => 'ProjectsController@delete'])->middleware('auth:sanctum');
@@ -38,14 +40,17 @@ Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], f
 
 
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
-    Route::apiResource('members', UsersController::class);
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    Route::apiResource('members', UserController::class);
     Route::apiResource('majors', MajorsController::class);
     Route::apiResource('courses', CoursesController::class);
+    Route::apiResource('types', TypesController::class);
     Route::apiResource('projects', ProjectsController::class);
-    Route::apiResource('projectMembers', ProjectMembersController::class);
-    Route::apiResource('tasks', TasksController::class);
-    Route::apiResource('blogs', BlogsController::class);
+    Route::apiResource('languages', LanguagesController::class);
+    Route::apiResource('frameworks', LanguagesController::class);
+    // Route::apiResource('projectMembers', ProjectMembersController::class);
+    // Route::apiResource('tasks', TasksController::class);
+    // Route::apiResource('blogs', BlogsController::class);
 
     Route::post('projectMembers/bulk', ['uses' => 'ProjectMembersController@bulkStore']);
 });
