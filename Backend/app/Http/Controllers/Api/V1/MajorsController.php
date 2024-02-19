47095525ccfb50c8e2d2e1fc80ee44b4847e1bd7
sharkvn1 <richberchannel01@gmail.com>
2024-02-19 15:29:41 +0700

    Cut: remove some function don't need

--- Backend/app/Http/Controllers/Api/V1/MajorsController.php
+++ Backend/app/Http/Controllers/Api/V1/MajorsController.php
@@ -27,14 +27,6 @@ class MajorsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreMajorsRequest $request)
@@ -43,26 +35,10 @@ class MajorsController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Majors $majors)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(UpdateMajorsRequest $request, $id)
     {
         Majors::where('Id', $id)->update($request->all());
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Majors $majors)
-    {
-        //
     }
 }
