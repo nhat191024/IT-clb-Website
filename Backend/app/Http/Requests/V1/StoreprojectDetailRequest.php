1375b21ddc7fa546c819754b714bf32ce0b049ed
sharkvn1 <richberchannel01@gmail.com>
2024-02-19 10:34:04 +0700

    Refactor: change class name form p to P

--- Backend/app/Http/Requests/V1/StoreprojectDetailRequest.php
+++ Backend/app/Http/Requests/V1/StoreprojectDetailRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateprojectDetailRequest extends FormRequest
+class StoreProjectDetailRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
