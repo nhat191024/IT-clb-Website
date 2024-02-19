b17169233e505a559696475a86149dd0d91c9da6
sharkvn1 <richberchannel01@gmail.com>
2024-02-19 10:19:02 +0700

    +Add: create pivot table for project framework relationship

---
+++ Backend/database/migrations/2024_02_19_021139_project_framework.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_framework', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('projects_id');
+            $table->unsignedBigInteger('frameworks_id');
+            $table->timestamps();
+
+            $table->foreign('projects_id')->references('id')->on('projects')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('frameworks_id')->references('id')->on('frameworks')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
