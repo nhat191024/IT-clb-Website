cde3367ce71ab743f6f430cd5f6e83bb77008260
sharkvn1 <richberchannel01@gmail.com>
2024-02-19 15:43:34 +0700

    Update: change name max length

--- Backend/database/migrations/2024_01_26_155800_create_frameworks_table.php
+++ Backend/database/migrations/2024_01_26_155800_create_frameworks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('frameworks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 30);
             $table->timestamps();
         });
     }
