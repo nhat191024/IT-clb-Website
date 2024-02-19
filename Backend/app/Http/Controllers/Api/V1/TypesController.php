98cf3a4d164023b23548dd8870510a21de45da53
sharkvn1 <richberchannel01@gmail.com>
2024-02-19 15:36:17 +0700

    Update: add basic function

--- Backend/app/Http/Controllers/Api/V1/TypesController.php
+++ Backend/app/Http/Controllers/Api/V1/TypesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\types;
 
+use App\Http\Requests\V1\StoreTypesRequest;
+use App\Http\Requests\V1\UpdateTypesRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\TypeResource;
 use App\Http\Resources\V1\TypeCollection;
@@ -14,6 +16,9 @@ use Illuminate\Http\Request;
 
 class TypesController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(Request $request)
     {
         $filter = new TypeData();
@@ -22,5 +27,39 @@ class TypesController extends Controller
         $types = types::where($filterItems);
 
         return new TypeCollection($types->paginate()->appends($request->query()));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypesRequest $request)
+    {
+        $result = new TypeResource(types::create($request->all()));
+        if ($result) {
+            return response()->json([
+                'message' => 'success',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'fail',
+            ], 400);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypesRequest $request, $id)
+    {
+        $result = types::where('Id', $id)->update($request->all());
+        if ($result) {
+            return response()->json([
+                'message' => 'success',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'fail',
+            ], 400);
+        }
     }
 }
