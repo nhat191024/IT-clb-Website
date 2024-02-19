933d8ff726aa5c3d70afdbb274663ed69d05d506
sharkvn1 <richberchannel01@gmail.com>
2024-02-19 15:29:09 +0700

    Update: add store and update function

--- Backend/app/Http/Controllers/Api/V1/LanguagesController.php
+++ Backend/app/Http/Controllers/Api/V1/LanguagesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\languages;
 
+use App\Http\Requests\V1\StoreLanguagesRequest;
+use App\Http\Requests\V1\UpdateLanguagesRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\LanguageCollection;
 use App\Http\Resources\V1\LanguageResource;
@@ -14,12 +16,49 @@ use Illuminate\Http\Request;
 
 class LanguagesController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(Request $request){
         $filter = new LanguageData();
         $filterItems = $filter->transform($request);
 
         $languages = languages::where($filterItems);
 
-        return new LanguageCollection($languages->paginate()->appends($request->query()));
+        return new LanguageCollection($languages->paginate(5)->appends($request->query()));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLanguagesRequest $request)
+    {
+        $result = new LanguageResource(languages::create($request->all()));
+        if ($result) {
+            return response()->json([
+                'message' => 'success',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'fail',
+            ], 400);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLanguagesRequest $request, $id)
+    {
+        $result = languages::where('Id', $id)->update($request->all());
+        if ($result) {
+            return response()->json([
+                'message' => 'success',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'fail',
+            ], 400);
+        }
     }
 }
