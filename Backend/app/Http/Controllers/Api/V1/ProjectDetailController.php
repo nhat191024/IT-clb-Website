97033f3a643697d6ed6e9ecd20756e66ac0eca75
sharkvn1 <richberchannel01@gmail.com>
2024-02-19 15:30:09 +0700

    Update: add basic function

--- Backend/app/Http/Controllers/Api/V1/ProjectDetailController.php
+++ Backend/app/Http/Controllers/Api/V1/ProjectDetailController.php
@@ -1,27 +1,32 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
 use App\Models\projectDetails;
+
+use App\Data\V1\PrjDetailData;
 use App\Http\Requests\V1\StoreProjectDetailRequest;
 use App\Http\Requests\V1\UpdateProjectDetailRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\ProjectDetailCollection;
+use App\Http\Resources\V1\ProjectDetailResource;
+use Illuminate\Http\Request;
 
 class ProjectDetailController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $filter = new PrjDetailData();
+        $filterItems = $filter->transform($request);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $project = projectDetails::where($filterItems);
+
+        $project = $project->with('leader', 'projectMember');
+
+        return new ProjectDetailCollection($project->paginate(5)->appends($request->query()));
     }
 
     /**
@@ -29,30 +34,51 @@ class ProjectDetailController extends Controller
      */
     public function store(StoreProjectDetailRequest $request)
     {
-        //
-    }
+        $projectDetail = projectDetails::create([
+            'leaderID' => $request->leaderID,
+            'projectID' => $request->projectID,
+            'description' => $request->description,
+            'projectSrc' => $request->projectSrc,
+            'status' => $request->status,
+            'show' => $request->show,
+            'startDate' => $request->startDate,
+            'endDate' => $request->endDate,
+        ]);
+        $projectDetail->projectMember()->attach($request->projectMember);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(projectDetails $projectDetail)
-    {
-        //
+        if ($projectDetail) {
+            return response()->json([
+                'message' => 'success',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'fail',
+            ], 400);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProjectDetailRequest $request, projectDetails $projectDetail)
+    public function update(UpdateProjectDetailRequest $request, $id)
     {
-        //
-    }
+        $projectDetail  = projectDetails::find($id);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(projectDetails $projectDetail)
-    {
-        //
+        $projectDetail->update($request->all());
+
+        if ($request->projectMember) {
+            $projectDetail->projectMember()->detach();
+            $projectDetail->projectMember()->attach($request->projectMember);
+        }
+
+        if ($projectDetail) {
+            return response()->json([
+                'message' => 'success',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'fail',
+            ], 400);
+        }
     }
 }
